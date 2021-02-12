7d79711e8fbc27dedc11ca66a4af606d386d975a
CyberCreatureSec <50676042+cybercreaturesec@users.noreply.github.com>
2021-02-12 13:52:13 -0800

    Updated Docs

--- example_handler.py
+++ example_handler.py
@@ -1,7 +1,53 @@
-from sonoma import defaultHandler, httpServer
+from sonoma import httpServer, defaults
+from http import HTTPStatus
 
+# DEFINE A CUSTOM HANDLER
+def myHandler(self, requestStatusLine, requestHeaders, requestBody, client_connection, client_address):
+    """
+    ## Supported Methods:
+    - GET
+    - HEAD
+    """
+
+    print("Client: %s\n" % str(client_address))
+
+    headerStrings = []
+    for header in requestHeaders:
+        headerStrings.append("%s: %s\n" % (header[0], header[1]))   
+
+    myCustomResponse = """
+        <!DOCTYPE html><html><head>
+        <style>html, body{ margin: 0 auto;text-align:center; }</style>
+        </head><body>
+        <h1 style=\"text-align:center;\">Hello World!</h1>
+        <span>This is a custom response from %s.</span>
+        <br/><br/>
+        <span>Request Headers:</span>
+        <br/>
+        <textarea cols="100" rows="100" style="width: 75%%;height: 100%%;margin: 0 auto;">%s</textarea>
+        </body></html>
+    """ % (defaults.serverName, "".join(headerStrings)) 
+    
+    # SERVE GET
+    if requestStatusLine.split()[0].lower() == "get":
+        responseStatusLine, responseHeaders = self.httpHeaders(HTTPStatus.OK, contentType="html")
+        responseBody = myCustomResponse
+        return (responseStatusLine, responseHeaders, responseBody)
+
+    # SERVE HEAD
+    elif requestStatusLine.split()[0].lower() == "head":   
+        responseStatusLine, responseHeaders = self.httpHeaders(HTTPStatus.OK, contentType="text")
+        return (responseStatusLine, responseHeaders, "")  
+    
+    # RESPOND WITH 405 STATUS - METHOD NOT ALLOWED
+    else:
+        responseStatusLine, responseHeaders = self.httpHeaders(HTTPStatus.METHOD_NOT_ALLOWED, contentType="text")
+        return (responseStatusLine, responseHeaders, "")   
+
+
+# INITIALIZE THE SERVER BUT SET OUR CUSTOM HANDLER BEFORE RUNNING.
 server = httpServer('127.0.0.1', 8888)
 
-server.handler = defaultHandler
+server.set_handler(myHandler)
 
 server.run()
--- sonoma/__init__.py
+++ sonoma/__init__.py
@@ -13,7 +13,7 @@ import email.utils as eut
 
 class defaults:
     httpVersion = "1.1" # DONT CHANGE THIS UNLESS YOU KNOW EXACLTY WHAT YOU ARE DOING!
-    serverName = "Sonoma/" + __version__.split('.', 1)[0] # SERVER NAME/VERSION. THIS WILL SHOW IN THE RESPONSE HEADER TO VISITORS. YOU CAN CHANGE THIS TO ANYTHING.
+    serverName = "Sonoma/" + __version__ # SERVER NAME/VERSION. THIS WILL SHOW IN THE RESPONSE HEADER TO VISITORS. YOU CAN CHANGE THIS TO ANYTHING.
     defaultOrigin = "*" # DEFAULT ORIGIN HEADER. THE HEADER WILL DISPLAY THIS VALUE UNLESS OTHERWISE CHANGED IN YOU CUSTOM HANDLER.
     # DEFAULT RESPONSE FOR DEFAULT HANDLER, YOU ARE ENCOURAGED TO CHANGE THIS IF YOU ARE NOT USING A CUSTOM HANDLER.
     defaultResponse = """ 
@@ -22,7 +22,7 @@ class defaults:
         </head><body>
         <h1 style=\"text-align:center;\">Hello World!</h1>
         <span>This is the default webpage for %s.</span>
-        </body></html>"
+        </body></html>
         """ % serverName 
 
 def defaultHandler(self, requestStatusLine, requestHeaders, requestBody, client_connection, client_address):
